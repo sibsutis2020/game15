84f854496f9ede778e42664db125a31eb7ca114e
Alexey54 <x-men54@mail.ru>
2020-04-26 14:24:59 +0700

    Implementation of canvas formation

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ y_count = 4
 
 #массив игровых элементов
 elems = ["e1", "e2", "e2","e3","e4","e5","e6","e7", "e8","e8","e9","e10","e11", "e12","e13","e14","e15","e0"]
+
+root = Tk()
+root.title("Пятнашки")
+
+canv = Canvas()
+canv.pack()
+
+root.mainloop()
