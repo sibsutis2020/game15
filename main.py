c1891342494b1d846c94d63a161b679931fbc99e
Alexey54 <x-men54@mail.ru>
2020-04-27 09:49:29 +0700

    Add getting

--- main.py
+++ main.py
@@ -12,8 +12,15 @@ elems = ["e1", "e2", "e2","e3","e4","e5","e6","e7", "e8","e8","e9","e10","e11", 
 canv_width = x_count * cell_size
 canv_height = y_count * cell_size
 
+#функция с удалением приветствия 
+def start():
+    f_top.pack_forget()
+
 root = Tk()
 root.title("Пятнашки")
+
+f_top = Frame()
+f_top.pack()
 
 canv = Canvas(root, width = canv_width, height = canv_height)
 canv.pack()
@@ -23,5 +30,11 @@ for x_num in range(x_count):
     for y_num in range(y_count):
         canv.create_line(0, y_num * cell_size, canv_width, y_num * cell_size)
         canv.create_line( x_num * cell_size, 0, x_num * cell_size, canv_height)
+
+greeting = Label(f_top, text = "Hello, Player!", font = ("Ubuntu", 20))
+greeting.pack()
+
+start = Button(text = "Start", font = ("Ubuntu", 15), command = start)
+start.pack()
 
 root.mainloop()
